4d2f629c1fcaa3be2250abe8321d9b161acee90d
Abraham Mwaura <62555334+Abraham-Mwaura@users.noreply.github.com>
2022-10-09 17:20:38 +0300

    complete code and a draft pure html file

--- Creating_a_Webserver_in_esp32.ino
+++ Creating_a_Webserver_in_esp32.ino
@@ -1,119 +1,128 @@
-//for wifi connectivity
 #include <WiFi.h>
-//to help create a webserver
-
-//To be able to use it, make sure you select the right board
 #include <WebServer.h>
+/* Put your SSID & Password */
 
-const char* ssid= "";
-const char* password ="";
+const char* ssid = ""; // Enter SSID here
+const char* password = "*********"; //Enter Password here
 
-//windows>cmd>ipconfig
-
-
-IPAddress local_ip(192,168,163,230);
-IPAddress gateway (192,168,163,13);
+/* Put IP Address details */
+IPAddress local_ip(192,168,0,165);
+IPAddress gateway(192,168,0,1);
 IPAddress subnet(255,255,255,0);
 
-//create the webserver object and declare the port
 WebServer server(80);
 
-uint8_t LEDPin1 =2;
-//initial state
-bool LED1status =LOW;
-uint8_t LEDPin2 =5;
-bool LED2status =LOW;
-
+uint8_t LEDOnePin = 2;
+bool LEDOneStatus = LOW;
+uint8_t LEDTwoPin = 5;
+bool LEDTwoStatus = LOW;
 
 void setup() {
-Serial.begin(115200);
-pinMode(LEDPin1, OUTPUT);
-pinMode(LEDPin2, OUTPUT);
-
-
-WiFi.softAP(ssid, password);
-WiFi.softAPConfig(local_ip,gateway,subnet);
-//to stabilize everything
-delay(100);
-
-//opens the specified function when the specified url is hit
-//192.168.163.230/led1on  
-server.on("/", handle_OnConnect);
-server.on("/led1on", handle_led1on);
-server.on("/led1off", handle_led1off);
-server.on("/led2on", handle_led2on);
-server.on("/led2off", handle_led2off);
-server.onNotFound(handle_NotFound);
-
-//to start the server
-server.begin();
-Serial.println("https server has started");
+ Serial.begin(115200);
+ pinMode(LEDOnePin, OUTPUT);
+ pinMode(LEDTwoPin, OUTPUT);
+ WiFi.softAP(ssid, password);
+ WiFi.softAPConfig(local_ip, gateway, subnet);
+ delay(100);
+ 
+ server.on("/", handle_OnConnect);
+ server.on("/led1on", handle_led1on);
+ server.on("/led1off", handle_led1off);
+ server.on("/led2on", handle_led2on);
+ server.on("/led2off", handle_led2off);
+ server.onNotFound(handle_NotFound);
+ 
+ server.begin();
+ Serial.println("HTTP server Has started");
 }
 
 void loop() {
-  
-  server.handleClient();
-  
-  if(LED1status) {
-  digitalWrite(LEDPin1,HIGH);
-  }
-  else{
-  digitalWrite(LEDPin1,LOW);
-  }
-
-  if(LED2status) {
-  digitalWrite(LEDPin2,HIGH);
-  }
-  else{
-  digitalWrite(LEDPin2,LOW);
-  }
+ server.handleClient();
+ if(LEDOneStatus)
+ {digitalWrite(LEDOnePin, HIGH);}
+ else
+ {digitalWrite(LEDOnePin, LOW);}
+ 
+ if(LEDTwoStatus)
+ {digitalWrite(LEDTwoPin, HIGH);}
+ else
+ {digitalWrite(LEDTwoPin, LOW);}
 }
 
 
-void handle_OnConnect(){
-  //turns off the two LEDs
-LED1status =  LOW;
-LED2status = LOW;
-
-Serial.println("GPIO 2: off and GPIO5: off");
-//google on https codes and their meaning
-
-server.send(200, "text/html", SendHTML(LED1status, LED2status));
+void handle_OnConnect() {
+ LEDOneStatus = LOW;
+ LEDTwoStatus = LOW;
+ Serial.println("GPIO2 Status: OFF | GPIO5 Status: OFF");
+ server.send(200, "text/html", SendHTML(LEDOneStatus,LEDTwoStatus)); 
 }
 
-void handle_led1on(){
-LED1status =  HIGH;
-Serial.println("GPIO 2: on");
-server.send(200, "text/html", SendHTML(LED1status,true ));
+void handle_led1on() {
+ LEDOneStatus = HIGH;
+ Serial.println("GPIO2 Status: ON");
+ server.send(200, "text/html", SendHTML(true,LEDTwoStatus)); 
 }
 
-void handle_led1off(){
-LED1status = LOW;
-Serial.println("GPIO 1: OFF");
-server.send(200, "text/html", SendHTML(LED1status, false ));
+void handle_led1off() {
+ LEDOneStatus = LOW;
+ Serial.println("GPIO2 Status: OFF");
+ server.send(200, "text/html", SendHTML(false,LEDTwoStatus)); 
 }
 
-void handle_led2on(){
-LED2status =  HIGH;
-Serial.println("GPIO 5: on");
-server.send(200, "text/html", SendHTML(LED2status, true ));
+
+void handle_led2on() {
+ LEDTwoStatus = HIGH;
+ Serial.println("GPIO5 Status: ON");
+ server.send(200, "text/html", SendHTML(LEDOneStatus,true)); 
 }
 
-void handle_led2off(){
-LED2status = LOW;
-Serial.println("GPIO 5: OFF");
-server.send(200, "text/html", SendHTML(LED2status, false ));
+void handle_led2off() {
+ LEDTwoStatus = LOW;
+ Serial.println("GPIO5 Status: OFF");
+ server.send(200, "text/html", SendHTML(LEDOneStatus,false)); 
 }
 
 void handle_NotFound(){
-
-Serial.println("Not FOUND");
-server.send(404, "text/html", "Not Found" );
+ server.send(404, "text/plain", "Not found");
 }
 
 
-String SendHTML (uint8_t led1stat, uint8_t led2stat)
-{
-  String ptr ="<!DOCTYPE html> <html>\n";
-  ptr + = "<head><meta name=\ "viewport\" content=\"width=device-width, initial-scale=1.0 user-scalable=no\">\n"; 
+String SendHTML(uint8_t led1stat,uint8_t led2stat){
+ String ptr = "<!DOCTYPE html> <html>\n";
+ ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initialscale=1.0, user-scalable=no\">\n";
+ ptr +="<title>LED Control</title>\n";
+ ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 
+0px auto; text-align: center;}\n";
+ ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} 
+h3 {color: #444444;margin-bottom: 50px;}\n";
+ ptr +=".button {display: block;width: 80px;background-color: 
+#3498db;border: none;color: white;padding: 13px 30px;text-decoration: 
+none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 
+4px;}\n";
+ ptr +=".button-on {background-color: #3498db;}\n";
+ ptr +=".button-on:active {background-color: #2980b9;}\n";
+ ptr +=".button-off {background-color: #34495e;}\n";
+ ptr +=".button-off:active {background-color: #2c3e50;}\n";
+ ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+ ptr +="</style>\n";
+ ptr +="</head>\n";
+ ptr +="<body>\n";
+ ptr +="<h1>ESP32 Web Server Text</h1>\n";
+ ptr +="<h3>Using AP Mode</h3>\n";
+ 
+ if(led1stat)
+ {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" 
+href=\"/led1off\">OFF</a>\n";}
+ else
+ {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" 
+href=\"/led1on\">ON</a>\n";}
+ if(led2stat)
+ {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" 
+href=\"/led2off\">OFF</a>\n";}
+ else
+ {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" 
+href=\"/led2on\">ON</a>\n";}
+ ptr +="</body>\n";
+ ptr +="</html>\n";
+ return ptr;
 }
